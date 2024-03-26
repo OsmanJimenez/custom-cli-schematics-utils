2ce160c6a986f0c441aeaf531c38cb8d553e2366
OsmanJimenez <osmanjimenez.dev@gmail.com>
2024-03-26 08:30:54 -0500

    Feat: enable create templates

--- src/architecture-clean-code/index.ts
+++ src/architecture-clean-code/index.ts
@@ -1,8 +1,35 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import {
+  apply,
+  branchAndMerge,
+  chain,
+  mergeWith,
+  Rule,
+  SchematicContext,
+  template,
+  Tree,
+  url,
+  move,
+} from '@angular-devkit/schematics';
+import { strings } from '@angular-devkit/core';
 
 export function architectureCleanCode(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    _context.logger.info('Running the architecture-clean-code schematic!');
-    return tree;
+  return (_tree: Tree, _context: SchematicContext) => {
+    // Verificar si la ruta proporcionada es válida o no
+    const path = _options.path ? _options.path : 'src/app'; // Usa la ruta proporcionada o el valor predeterminado
+
+    const sourceTemplates = url('./files'); // La ruta a tus plantillas
+    const sourceParametrizedTemplates = apply(sourceTemplates, [
+      template({
+        ..._options,
+        ...strings, // Funciones de strings para manipulación
+      }),
+      move(path), // Usa la ruta del usuario o el valor predeterminado
+    ]);
+
+    return chain([
+      branchAndMerge(chain([
+        mergeWith(sourceParametrizedTemplates),
+      ])),
+    ]);
   };
 }
